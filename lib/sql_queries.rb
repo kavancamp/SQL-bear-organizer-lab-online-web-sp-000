1a4402e0f5867bbc078784905e91befb6d969456
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-17 15:17:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "-SELECT bears.name, bears.age WHERE alive = 1 ORDER BY age ASC"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT bears.name, bears.age FROM bears ORDER BY age ASC LIMIT 1"
 end
 
 def selects_most_prominent_color_and_returns_with_count
